ed18a066c4aa92c7e6502125566c6e015ea6cbaa
Alyssa Yates <bosslys@icloud.com>
2019-11-22 21:46:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,21 @@
 class Application
 
   def call(env)
-    resp = Rack::Time.now
+    resp = Rack::Time.new
+
+    morning = Kernel.rand(0..12)
+    afternoon = Kernel.rand(12..24)
+
+    resp.write "#{morning}\n"
+    resp.write "#{afternoon}\n"
+
+    if morning==evening
+        resp.write "Good Morning"
+      else
+        resp.write "Good Afternoon"
+      end
+
+      resp.finish
+    end
+
   end
-end
