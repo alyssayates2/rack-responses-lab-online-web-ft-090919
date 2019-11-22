f94a755a6295213da08f658449486035d3f46a05
Alyssa Yates <bosslys@icloud.com>
2019-11-22 21:52:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,19 +16,4 @@ class Application
     resp.finish
   end
 
-    # morning = Kernel.rand(0..12)
-    # afternoon = Kernel.rand(12..24)
-    #
-    # resp.write "#{morning}\n"
-    # resp.write "#{afternoon}\n"
-    #
-    # if morning==evening
-    #     resp.write "Good Morning"
-    #   else
-    #     resp.write "Good Afternoon"
-    #   end
-    #
-    #   resp.finish
-    # end
-
   end
